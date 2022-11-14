4e2e38ce8a568e22f02cf09c90179747e98ad24c
tucno21 <carlitostucno@gmail.com>
2022-11-13 19:20:03 -0500

    fix button

--- public/assets/js/users.js
+++ public/assets/js/users.js
@@ -54,7 +54,7 @@ async function generarDataTable() {
     i++;
     element["estado"] =
       element.status == 1
-        ? `<p data-url="${urlStatus}?id=${element.id}" class="btnEstado btn btn-outline-success rounded-pill btn-sm p-0 px-1">Habilitado</p>`
+        ? `<button data-url="${urlStatus}?id=${element.id}" class="btnEstado btn btn-outline-success rounded-pill btn-sm p-0 px-1">Habilitado</button>`
         : `<p data-url="${urlStatus}?id=${element.id}" class="btnEstado btn btn-outline-danger rounded-pill btn-sm p-0 px-1">Inhabilitado</p>`;
     element["actions"] = `
         <a href="${urlEdit}?id=${element.id}" class="btn btn-outline-warning btn-sm btnEditar">
