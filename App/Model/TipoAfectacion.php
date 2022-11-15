3e48c93eb7fe41e37916a917f71272265f1fa9d2
tucno21 <carlitostucno@gmail.com>
2022-11-14 20:33:01 -0500

    add model tipo afectacio

---
+++ App/Model/TipoAfectacion.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Model;
+
+use System\Model;
+
+class TipoAfectacion extends Model
+{
+    /**
+     * nombre de la tabla
+     */
+    protected static $table       = 'tipo_afectacion';
+    /**
+     * nombre primary key
+     */
+    protected static $primaryKey  = 'id';
+    /**
+     * nombre de la columnas de la tabla
+     */
+    protected static $allowedFields = ['codigo', 'descripcion', 'codigo_afectacion', 'nombre_afectacion', 'tipo_afectacion', 'estado'];
+    /**
+     * obtener los datos de la tabla en 'array' u 'object'
+     */
+    protected static $returnType     = 'object';
+    /**
+     * si hay un campo de contraseña cifrar (true/false)
+     */
+    protected static $passEncrypt = false;
+
+    protected static $useTimestamps   = false;
+    /**
+     * $createdField debe ser DATETIME o TIMESTAMPS con condicion null
+     * $$updatedField debe ser TIMESTAMPS con condicion null
+     * el framework se encarga de enviar las fechas y no BD
+     * colocar el nombre de los campos de fecha de la BD
+     */
+    protected static $createdField    = 'created_at';
+    protected static $updatedField    = 'updated_at';
+}
